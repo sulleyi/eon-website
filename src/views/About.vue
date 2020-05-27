e21c56391729070e17eb5339eb0ec0f40673c954
sulleyi <57299945+sulleyi@users.noreply.github.com>
2020-05-27 11:31:24 -0400

    playing around with About page styling

--- src/views/About.vue
+++ src/views/About.vue
@@ -5,84 +5,130 @@
         <div class="header">
           <h1>Entrepreneurs of the Nott (EON) is a group that looks to...</h1>
         </div>
-        <div class="taglines">
-          <div>
-            <h2><strong>inspire</strong> design thinking</h2>
-          </div>
-          <div>
-            <h2><strong>empower</strong> through ideation and hands-on projects</h2>
-          </div>
-          <div>
-            <h2><strong>create change</strong> within and outside of Union.</h2>
-          </div>
-        </div>
+
         <div class="mission-statements">
-            <p>
-              We <strong>inspire</strong> by encouraging campus-wide collaboration at Union
-              and exposing the student body to an entrepreneurial mindset,
-              demonstrating that the ambiguous concept of “entrepreneurship”,
-              (and the skill sets that come with it) has more personal and
-              professional application than commonly thought.
-            </p>
-
-            <p>
-              We <strong>empower</strong> through speaker series, providing a platform
-              for students to connect with alumni, the local community,
-              and resources to see their ideas materialize into tangible
-              results and/or products.
-            </p>
-
-
-            <p>
-              We <strong>create change</strong> through weekly “Think Tanks” workshop sessions,
-              have monthly meetings, hold design thinking workshops, and trips
-              to local businesses.
-            </p>
-
+          <div class="statement1">
+            <div
+            @mouseover="hover1 = true"
+            @mouseleave="hover1 = false"
+            >
+              <h2><strong>inspire</strong> design thinking</h2>
+            </div>
+            <div v-if="hover1">
+              <p>
+                We <strong>inspire</strong> by encouraging campus-wide collaboration at Union
+                and exposing the student body to an entrepreneurial mindset,
+                demonstrating that the ambiguous concept of “entrepreneurship”,
+                (and the skill sets that come with it) has more personal and
+                professional application than commonly thought.
+              </p>
+            </div>
+          </div>
+          <div class="statement2">
+            <div
+            @mouseover="hover2 = true"
+            @mouseleave="hover2 = false"
+            >
+              <h2><strong>empower</strong> through ideation and hands-on projects</h2>
+            </div>
+            <div class="tag" v-if="hover2">
+              <p>
+                We <strong>empower</strong> through speaker series, providing a platform
+                for students to connect with alumni, the local community,
+                and resources to see their ideas materialize into tangible
+                results and/or products.
+              </p>
+            </div>
+          </div>
+          <div class="statement3">
+            <div
+            @mouseover="hover3 = true"
+            @mouseleave="hover3 = false"
+            >
+              <h2><strong>create change</strong> within and outside of Union.</h2>
+            </div>
+            <div v-if="hover3">
+              <p>
+                We <strong>create change</strong> through weekly “Think Tanks” workshop sessions,
+                have monthly meetings, hold design thinking workshops, and trips
+                to local businesses.
+              </p>
+            </div>
+          </div>
         </div>
       </div>
     </section>
   </div>
 </template>
 
+<script>
+
+export default{
+  data(){
+    return{
+      hover1: false,
+      hover2: false,
+      hover3: false
+    };
+  }
+}
+</script>
 
 <style scoped>
 .content{
   margin: 10px;
-  background: rgb(255, 255, 255);
-  background: linear-gradient(
-    -157deg,
-    rgba(255, 255, 255, 1) 0%,
-    rgba(255, 255, 255, 1) 74%,
-    #2c3e50 74%,
-    #2c3e50 100%
-  );
+
 }
 .header{
   text-align: left;
   padding: 10px;
 }
-.taglines{
-  padding: 25px;
-  border: 2px;
-  border-radius: 10px;
-}
-.taglines > div:first-child {
-    text-align: left;
-}
-.taglines > div:last-child {
-text-align: right;
-}
+
+
 .mission-statements{
+  vertical-align: center;
+  margin: auto;
+  display:block;
+  width: 100%;
   padding: 25px;
-  margin-bottom: 250px;
+  padding-bottom: 300px;
+
 }
-.mission-statements > p:first-child{
-  text-align: left;
+.statement1{
+  display: block;
+  background-color:grey;
+  width:300px;
+  margin:20px;
+  padding-bottom:25px;
+  background-color: lightgrey;
+  border: 5px;
+  border-style: outset;
+  border-color: grey;
 }
-.mission-statements > p:last-child{
-  text-align: right;
+
+.statement2{
+  display: block;
+  background-color: grey;
+  margin: 20px;
+  width: 300px;
+  padding-bottom: 25px;
+  background-color: lightgrey;
+  border: 5px;
+  border-style: outset;
+  border-color: grey;
 }
+
+.statement3{
+  display: block;
+  background-color: lightgrey;
+  border: 5px;
+  border-style: outset;
+  border-color: grey;
+  width:300px;
+  margin: 20px;
+  padding-bottom: 25px;
+}
+
 
 
 </style>
